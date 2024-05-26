3358bd1e451141a65a91bd9ff546a7cc9cae00ff
Radha Patel <rrpatel@mit.edu>
2024-05-25 22:57:04 -0400

    Use TACO column major for MTTKRP

--- kernels/mttkrp/run_mttkrp.jl
+++ kernels/mttkrp/run_mttkrp.jl
@@ -13,8 +13,7 @@ include("mttkrp_taco.jl")
 include("mttkrp_splatt.jl")
 
 n = 500
-rank = [10, 100, 250, 500]
-sparsities = [0.1, 0.075, 0.05, 0.025, 0.01, 0.0075, 0.005, 0.0025, 0.0001]
+rank_sparsity = [(10, 0.1), (10, 0.0001), (500, 0.1), (500, 0.0001)]
 methods = Dict(
     "mttkrp_finch_ref" => mttkrp_finch_ref,
     "mttkrp_finch_opt" => mttkrp_finch_opt,
@@ -23,47 +22,45 @@ methods = Dict(
 )
 
 results = []
-for r in rank
-    for sp in sparsities
-        triA = fsprand(n, n, n, sp)
-        A = [triA[sort([i, j, k])...] for i = 1:n, j = 1:n, k = 1:n]
-        # A = bspread("../../data/symmetric_n$(n)_sp$(sp).bsp.h5")
-        B = rand(n, r)   
-        C = zeros(n, r)
-        C_ref = nothing
-        for (key, method) in methods
-            @info "testing" key n sp r
-            res = method(C, A, B)
-            time = res.time
-            C_res = nothing
+for (r, sp) in rank_sparsity
+    triA = fsprand(n, n, n, sp)
+    A = [triA[sort([i, j, k])...] for i = 1:n, j = 1:n, k = 1:n]
+    # A = bspread("../../data/symmetric_n$(n)_sp$(sp).bsp.h5")
+    B = rand(n, r)   
+    C = zeros(n, r)
+    C_ref = nothing
+    for (key, method) in methods
+        @info "testing" key n sp r
+        res = method(C, A, B)
+        time = res.time
+        C_res = nothing
+        nondiag_time = nothing
+        diag_time = nothing
+        try
+            nondiag_time = res.nondiag_time
+            diag_time = res.diag_time
+        catch
             nondiag_time = nothing
             diag_time = nothing
-            try
-                nondiag_time = res.nondiag_time
-                diag_time = res.diag_time
-            catch
-                nondiag_time = nothing
-                diag_time = nothing
-            end
-            try
-                C_res = res.C.C
-            catch
-                C_res = res.C
-            end
-            C_ref = something(C_ref, C_res)
-            norm(C_res - C_ref)/norm(C_ref) < 0.1 || @warn("incorrect result via norm")
-
-            @info "results" time
-            push!(results, OrderedDict(
-                "time" => time,
-                "nondiag_time" => nondiag_time,
-                "diag_time" => diag_time,
-                "method" => key,
-                "sparsity" => sp,
-                "size" => n,
-                "rank" => r,
-            ))
-            write("mttkrp_results.json", JSON.json(results, 4))
         end
+        try
+            C_res = res.C.C
+        catch
+            C_res = res.C
+        end
+        C_ref = something(C_ref, C_res)
+        norm(C_res - C_ref)/norm(C_ref) < 0.1 || @warn("incorrect result via norm")
+
+        @info "results" time
+        push!(results, OrderedDict(
+            "time" => time,
+            "nondiag_time" => nondiag_time,
+            "diag_time" => diag_time,
+            "method" => key,
+            "sparsity" => sp,
+            "size" => n,
+            "rank" => r,
+        ))
+        write("mttkrp_results.json", JSON.json(results, 4))
     end
 end
