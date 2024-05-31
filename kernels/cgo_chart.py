a4b2e3230919d46b8ef95e8490b76e1b49f12961
Radha Patel <rrpatel@mit.edu>
2024-05-30 20:13:38 -0400

    Update charts and results

--- kernels/cgo_chart.py
+++ kernels/cgo_chart.py
@@ -21,7 +21,7 @@ def use_sparsity_rank(result):
     return f"{s, r}"
 
 
-def all_formats_chart(results_filepath, get_matrix_name, reference, optimized, methods, colors, title, y_label, x_label, legend_labels, ordered = True, width=12):
+def all_formats_chart(results_filepath, get_matrix_name, reference, optimized, methods, colors, title, y_label, x_label, legend_labels, ordered = True, width=12, expected_speedup=0):
     results = json.load(open(results_filepath, 'r'))
     data = defaultdict(lambda: defaultdict(int))
 
@@ -48,10 +48,10 @@ def all_formats_chart(results_filepath, get_matrix_name, reference, optimized, m
 
     ordered_mtxs = [mtx for mtx, _ in ordered_data]
     short_mtxs = [mtx.rsplit('/',1)[-1] for mtx in ordered_mtxs]
-    make_grouped_bar_chart(methods, short_mtxs, all_data, colors=colors, title=title, y_label=y_label, x_label=x_label, legend_labels=legend_labels, plot_width=width)
+    make_grouped_bar_chart(methods, short_mtxs, all_data, colors=colors, title=title, y_label=y_label, x_label=x_label, legend_labels=legend_labels, plot_width=width, expected_speedup=expected_speedup)
 
 
-def make_grouped_bar_chart(labels, x_axis, data, colors = None, labeled_groups = [], title = "", y_label = "", x_label = "", bar_labels_dict={}, legend_labels=None, reference_label = "", ref_line = True, plot_width = 12):
+def make_grouped_bar_chart(labels, x_axis, data, colors = None, labeled_groups = [], title = "", y_label = "", x_label = "", bar_labels_dict={}, legend_labels=None, reference_label = "", ref_line = True, plot_width = 12, expected_speedup=0):
     horizontal_scale = 1
     fontfamily = "serif"
 
@@ -70,20 +70,25 @@ def make_grouped_bar_chart(labels, x_axis, data, colors = None, labeled_groups =
         ax.bar_label(rects, padding=0, labels=bar_labels, fontsize=4, rotation=90)
         multiplier += 1
 
-    ax.set_ylabel(y_label, fontsize=10)
-    ax.set_title(title, fontsize=12, fontfamily=fontfamily)
-    ax.set_xlabel(x_label)
+    if y_label:
+        ax.set_ylabel(y_label, fontsize=14)
+    # ax.set_title(title, fontsize=12, fontfamily=fontfamily)
+    if x_label:
+        ax.set_xlabel(x_label, fontsize=14)
     ax.set_xticks(x + width * (len(labels) - 1)/2, x_axis)
-    ax.tick_params(axis='x', which='major', labelsize=7.5, labelrotation=60, labelfontfamily=fontfamily)
-    ax.tick_params(axis='y', labelfontfamily=fontfamily)
-    ax.legend(legend_labels, loc='upper right', ncols=1, fontsize='small')
-    ax.set_ylim(0, max_height + 0.25 * max_height)
+    ax.tick_params(axis='x', which='major', labelsize=11, labelrotation=60, labelfontfamily=fontfamily)
+    ax.tick_params(axis='y', labelfontfamily=fontfamily, labelsize=10)
+    if legend_labels:
+        ax.legend(legend_labels, loc='upper right', ncols=1, fontsize=12)
+    ax.set_ylim(0, max_height + 0.05 * max_height)
 
         # Adjusting x-axis limits to make bars go to the edges
     ax.set_xlim(-0.5, (len(x_axis) - 0.5 + width * len(labels)) * horizontal_scale)
 
     if ref_line:
-        plt.plot([-1, len(x_axis)], [1, 1], linestyle='--', color="tab:red", linewidth=1.25, label=reference_label)
+        plt.plot([-1, len(x_axis)], [1, 1], linestyle='--', color="tab:red", linewidth=2, label=reference_label)
+    if expected_speedup:
+        plt.plot([-1, len(x_axis)], [expected_speedup, expected_speedup], linestyle='--', color="purple", linewidth=1.25, label=reference_label)
 
     fig_file = title.lower().replace(" ", "_") + ".png"
     plt.savefig(CHARTS_DIRECTORY + fig_file, dpi=200, bbox_inches="tight")
@@ -91,12 +96,13 @@ def make_grouped_bar_chart(labels, x_axis, data, colors = None, labeled_groups =
 
 
 all_formats_chart("ssymv/cgo_ssymv_results.json", use_suitesparse_name, "ssymv_finch_ref", "ssymv_finch_opt",
-                ["ssymv_finch_opt", "ssymv_taco"],  
-                {"ssymv_finch_opt": "tab:blue", "ssymv_taco": "tab:green"},
+                ["ssymv_finch_opt", "ssymv_taco", "ssymv_mkl"],  
+                {"ssymv_finch_opt": "tab:blue", "ssymv_taco": "tab:orange", "ssymv_mkl": "tab:green"},
                 "SSYMV Performance",
                 "Speedup",
                 "Matrix Name",
-                ["Symmetric Finch", "TACO"])
+                ["SySTeC", "TACO", "MKL"],
+                expected_speedup = 2)
 
 all_formats_chart("ssyrk/cgo_ssyrk_results.json", use_suitesparse_name, "ssyrk_ref", "ssyrk_opt",
                 ["ssyrk_opt"],  
@@ -104,55 +110,61 @@ all_formats_chart("ssyrk/cgo_ssyrk_results.json", use_suitesparse_name, "ssyrk_r
                 "SSYRK Performance",
                 "Speedup",
                 "Matrix Name",
-                ["Symmetric Finch"])
+                ["SySTeC"],
+                expected_speedup = 2)
 
 all_formats_chart("syprd/cgo_syprd_results.json", use_suitesparse_name, "syprd_ref", "syprd_opt",
                 ["syprd_opt", "syprd_taco"],  
-                {"syprd_opt": "tab:blue", "syprd_taco": "tab:green"},
+                {"syprd_opt": "tab:blue", "syprd_taco": "tab:orange"},
                 "SYPRD Performance",
                 "Speedup",
                 "Matrix Name",
-                ["Symmetric Finch", "TACO"])
+                ["SySTeC", "TACO"],
+                expected_speedup = 2)
 
 all_formats_chart("ttm/cgo_ttm_results.json", use_sparsity_rank, "ttm_finch_ref", "ttm_finch_opt",
                 ["ttm_finch_opt", "ttm_taco"],  
-                {"ttm_finch_opt": "tab:blue", "ttm_taco": "tab:green"},
+                {"ttm_finch_opt": "tab:blue", "ttm_taco": "tab:orange"},
                 "TTM Performance",
                 "Speedup",
                 "Sparsity, Numerical Rank",
-                ["Symmetric Finch", "TACO"],
+                ["SySTeC", "TACO"],
                 ordered = False,
-                width = 4)
+                width = 4,
+                expected_speedup = 2)
 
 all_formats_chart("mttkrp/cgo_mttkrp_dim3.json", use_sparsity_rank, "mttkrp_finch_ref", "mttkrp_finch_opt",
                 ["mttkrp_finch_opt", "mttkrp_taco", "mttkrp_splatt"],  
-                {"mttkrp_finch_opt": "tab:blue", "mttkrp_taco": "tab:green", "mttkrp_splatt": "tab:gray"},
+                {"mttkrp_finch_opt": "tab:blue", "mttkrp_taco": "tab:orange", "mttkrp_splatt": "tab:green"},
                 "3D MTTKRP Performance",
                 "Speedup",
-                "Sparsity, Numerical Rank",
-                ["Symmetric Finch", "TACO", "SPLATT"],
+                "", #"Sparsity, Numerical Rank",
+                [], # ["SySTeC", "TACO", "SPLATT"], 
                 ordered = False,
-                width = 4)
+                width = 4,
+                expected_speedup = 2)
 
 all_formats_chart("mttkrp/cgo_mttkrp_dim4.json", use_sparsity_rank, "mttkrp_finch_ref", "mttkrp_finch_opt",
                 ["mttkrp_finch_opt", "mttkrp_taco", "mttkrp_splatt"],  
-                {"mttkrp_finch_opt": "tab:blue", "mttkrp_taco": "tab:green", "mttkrp_splatt": "tab:gray"},
+                {"mttkrp_finch_opt": "tab:blue", "mttkrp_taco": "tab:orange", "mttkrp_splatt": "tab:green"},
                 "4D MTTKRP Performance",
-                "Speedup",
-                "Sparsity, Numerical Rank",
-                ["Symmetric Finch", "TACO", "SPLATT"],
+                "", #"Speedup",
+                "", #"Sparsity, Numerical Rank",
+                [], # ["SySTeC", "TACO", "SPLATT"],
                 ordered = False,
-                width = 4)
+                width = 4,
+                expected_speedup = 6)
 
 all_formats_chart("mttkrp/cgo_mttkrp_dim5.json", use_sparsity_rank, "mttkrp_finch_ref", "mttkrp_finch_opt",
                 ["mttkrp_finch_opt", "mttkrp_taco", "mttkrp_splatt"],  
-                {"mttkrp_finch_opt": "tab:blue", "mttkrp_taco": "tab:green", "mttkrp_splatt": "tab:gray"},
+                {"mttkrp_finch_opt": "tab:blue", "mttkrp_taco": "tab:orange", "mttkrp_splatt": "tab:green"},
                 "5D MTTKRP Performance",
-                "Speedup",
-                "Sparsity, Numerical Rank",
-                ["Symmetric Finch", "TACO", "SPLATT"],
+                "", #"Speedup",
+                "", #"Sparsity, Numerical Rank",
+                ["SySTeC", "TACO", "SPLATT"],
                 ordered = False,
-                width = 4)
+                width = 4,
+                expected_speedup = 24)
 
 # all_formats_chart("mttkrp/cgo_mttkrp_results_sparsity.json", use_sparsity, "mttkrp_finch_ref", "mttkrp_finch_opt",
 #                 ["mttkrp_finch_opt", "mttkrp_taco", "mttkrp_splatt"],  
@@ -160,7 +172,7 @@ all_formats_chart("mttkrp/cgo_mttkrp_dim5.json", use_sparsity_rank, "mttkrp_finc
 #                 "3D MTTKRP Performance",
 #                 "Speedup",
 #                 "Sparsity",
-#                 ["Symmetric Finch", "TACO", "SPLATT"],
+#                 ["SySTeC", "TACO", "SPLATT"],
 #                 ordered = False,
 #                 width = 4)
 
@@ -170,7 +182,7 @@ all_formats_chart("mttkrp/cgo_mttkrp_dim5.json", use_sparsity_rank, "mttkrp_finc
 #                 "4D MTTKRP Performance",
 #                 "Speedup",
 #                 "Sparsity",
-#                 ["Symmetric Finch", "TACO", "SPLATT"],
+#                 ["SySTeC", "TACO", "SPLATT"],
 #                 ordered = False,
 #                 width = 4)
 
@@ -180,6 +192,6 @@ all_formats_chart("mttkrp/cgo_mttkrp_dim5.json", use_sparsity_rank, "mttkrp_finc
 #                 "5D MTTKRP Performance",
 #                 "Speedup",
 #                 "Sparsity",
-#                 ["Symmetric Finch", "TACO", "SPLATT"],
+#                 ["SySTeC", "TACO", "SPLATT"],
 #                 ordered = False,
 #                 width = 4)
